85ff2dead7a495c1cee4c26b945d2a9344cd20f3
Spore Engineering <65569953+spore-engineering@users.noreply.github.com>
2021-04-04 11:31:21 -0400

    removed IERC20 library and ReentrancyGuard contract, not needed

--- Spore1155NFT.sol
+++ Spore1155NFT.sol
@@ -305,19 +305,6 @@ interface IAccessControl {
     function renounceRole(bytes32 role, address account) external;
 }
 
-interface IERC20 {
-
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
 interface IERC165 {
     function supportsInterface(bytes4 interfaceId) external view returns (bool);
 }
@@ -362,27 +349,6 @@ interface IERC1155Receiver is IERC165 {
 
 interface IERC1155MetadataURI is IERC1155 {
     function uri(uint256 id) external view returns (string memory);
-}
-
-abstract contract ReentrancyGuard {
-    uint256 private constant _NOT_ENTERED = 1;
-    uint256 private constant _ENTERED = 2;
-
-    uint256 private _status;
-
-    constructor () {
-        _status = _NOT_ENTERED;
-    }
-
-    modifier nonReentrant() {
-        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
-
-        _status = _ENTERED;
-
-        _;
-        
-        _status = _NOT_ENTERED;
-    }
 }
 
 abstract contract Context {
